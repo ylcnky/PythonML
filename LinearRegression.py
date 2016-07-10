6a3b192d29118bc2c752ad86a2742bbc395799f7
ylcnky <mehmet.yalcinkaya@aalto.fi>
2016-07-10 18:52:06 -0400

    best fit slope

--- LinearRegression.py
+++ LinearRegression.py
@@ -18,7 +18,8 @@ df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.
 df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
 forecast_col = 'Adj. Close'
 df.fillna(value=-99999, inplace=True)
-forecast_out = int(math.ceil(0.01 * len(df)))
+forecast_out = int(math.ceil(0.1 * len(df)))
+
 df['label'] = df[forecast_col].shift(-forecast_out)
 
 X = np.array(df.drop(['label'], 1))
@@ -31,16 +32,14 @@ df.dropna(inplace=True)
 y = np.array(df['label'])
 
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
-
-clf = LinearRegression(n_jobs=-1)
-clf.fit(X_train, y_train)
-with open('linearregression.pickle', 'wb') as f:
-	pickle.dump(clf, f)
-
-pickle_in = open('linearregression.pickle', 'rb')
+#COMMENTED OUT:
+##clf = svm.SVR(kernel='linear')
+##clf.fit(X_train, y_train)
+##confidence = clf.score(X_test, y_test)
+##print(confidence)
+pickle_in = open('linearregression.pickle','rb')
 clf = pickle.load(pickle_in)
 
-confidence = clf.score(X_test, y_test)
 
 forecast_set = clf.predict(X_lately)
 df['Forecast'] = np.nan
@@ -54,7 +53,6 @@ for i in forecast_set:
     next_date = datetime.datetime.fromtimestamp(next_unix)
     next_unix += 86400
     df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)]+[i]
-
 df['Adj. Close'].plot()
 df['Forecast'].plot()
 plt.legend(loc=4)
