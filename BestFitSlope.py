8130488b0982779237b9830390e62bf41910c9ed
ylcnky <mehmet.yalcinkaya@aalto.fi>
2016-07-10 20:00:33 -0400

    test with dummy data

--- BestFitSlope.py
+++ BestFitSlope.py
@@ -2,11 +2,27 @@ from statistics import mean
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import style
+import random
 
 style.use('fivethirtyeight')
 
-xs = np.array([1,2,3,4,5], dtype=np.float64)
-ys = np.array([5,4,6,5,6], dtype=np.float64)
+#xs = np.array([1,2,3,4,5], dtype=np.float64)
+#ys = np.array([5,4,6,5,6], dtype=np.float64)
+
+def create_dataset(hm, variance, step=2, correlation=False):
+	val = 1
+	ys = []
+	for i in range(hm):
+		y = val + random.randrange(-variance, variance)
+		ys.append(y)
+		if correlation and correlation == 'pos':
+			val += step
+		elif correlation and correlation == 'neg':
+			val -= step
+	xs = [i for i in range(len(ys))]
+
+
+	return np.array(xs, dtype=np.float64), np.array(ys, dtype=np.float64)
 
 def best_fit_slope_and_intercept(xs,ys):
     m = (((mean(xs)*mean(ys)) - mean(xs*ys)) /
@@ -24,6 +40,9 @@ def coefficient_of_determination(ys_orig, ys_line):
 	squared_error_y_mean = squared_error(ys_orig, y_mean_line)
 	return 1 - (squared_error_regr / squared_error_y_mean)
 
+xs, ys = create_dataset(40, 80, 2, correlation='pos')
+
+
 m, b = best_fit_slope_and_intercept(xs,ys)
 
 regression_line = [(m*x)+b for x in xs]
@@ -35,6 +54,6 @@ r_squared = coefficient_of_determination(ys, regression_line)
 print (r_squared)
 
 plt.scatter(xs,ys)
-plt.scatter(predict_x, predict_y, color='g')
+plt.scatter(predict_x, predict_y, s=100, color='g')
 plt.plot(xs, regression_line)
 plt.show()
