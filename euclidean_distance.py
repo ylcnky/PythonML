fe0962988373b9a550f66e4bf955a80b81239b1b
ylcnky <mehmet.yalcinkaya@aalto.fi>
2016-07-12 23:04:15 +0300

    final k nearest

--- euclidean_distance.py
+++ euclidean_distance.py
@@ -5,10 +5,9 @@ from collections import Counter
 import pandas as pd
 import random
 
-
 def k_nearest_neighbors(data, predict, k=3):
     if len(data) >= k:
-        warning.warn('K is set to a value less than total voting groups')
+        warnings.warn('K is set to a value less than total voting groups!')
     distances = []
     for group in data:
         for features in data[group]:
@@ -16,17 +15,19 @@ def k_nearest_neighbors(data, predict, k=3):
             distances.append([euclidean_distance, group])
 
     votes = [i[1] for i in sorted(distances)[:k]]
-    #print(Counter(votes).most_common(1))
     vote_result = Counter(votes).most_common(1)[0][0]
-    return vote_result
+    confidence = Counter(votes).most_common(1)[0][1] / k
 
-df = pd.read_csv('breast-cancer-wisconsin.data.txt')
-df.replace('?', -99999, inplace=True)
-df.drop(['id'],1,inplace=True)
+    return vote_result, confidence
+
+
+df = pd.read_csv("breast-cancer-wisconsin.data.txt")
+df.replace('?',-99999, inplace=True)
+df.drop(['id'], 1, inplace=True)
 full_data = df.astype(float).values.tolist()
 random.shuffle(full_data)
 
-test_size = 0.2
+test_size = 0.4
 train_set = {2:[], 4:[]}
 test_set = {2:[], 4:[]}
 train_data = full_data[:-int(test_size*len(full_data))]
@@ -43,9 +44,8 @@ total = 0
 
 for group in test_set:
     for data in test_set[group]:
-        vote = k_nearest_neighbors(train_set, data, k=5)
+        vote,confidence = k_nearest_neighbors(train_set, data, k=5)
         if group == vote:
-            correct +=1
-        total+=1
-
-print('Accuracy: ', correct/total)
+            correct += 1
+        total += 1
+print('Accuracy:', correct/total)
